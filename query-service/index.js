44db6cce80134f4ad6454e12a2dfa0e84c25b2a8
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-20 21:56:19 -0300

    feat: add event handler statement  in query service

--- query-service/index.js
+++ query-service/index.js
@@ -10,19 +10,25 @@ app.use(cors())
 const posts = {}
 
 app.get('/posts', (req, res) => {
-
+  res.send(posts)
 })
 
 app.post('/events', (req, res) => {
   const { type, data } = req.body
 
   if (type === 'PostCreated') {
+    const { id, title } = data
 
+    posts[id] = { id, title, comments: [] }
   }
 
-  if (type === 'CommentedCreated') {
-    
+  if (type === 'CommentCreated') {
+    const  { id, content, postId } = data
+
+    const post = posts[postId]
+    post.comments.push({ id , content })
   }
+
 })
 
 app.listen(4002, () => {
