e48dfe3076453021d140c5eede406f2d9d491b54
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-22 16:43:24 -0300

    feat: add event-bus-srv endpoint for event bus access

--- comments-service/index.js
+++ comments-service/index.js
@@ -23,7 +23,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   comments.push({ id: commentId, content, status: 'pending'})
   commentsByPostId[req.params.id] = comments
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'CommentCreated',
     data: {
       id: commentId,
@@ -48,7 +48,7 @@ app.post('/events', async (req, res) => {
     const comment = comments.find(comment => comment.id === id)
     comment.status = status
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
       type: 'CommentUpdated',
       data: {
         id, status, postId, content
